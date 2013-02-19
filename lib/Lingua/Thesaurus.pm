ef953e9cdb5b10f10d7d35d76f5820f2c8b7c275
Laurent Dami <dami@cpan.org>
2013-02-19 03:51:14 +0000

    Import of DAMI/Lingua-Thesaurus-0.02 from CPAN.
    gitpan-cpan-distribution: Lingua-Thesaurus
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         DAMI/Lingua-Thesaurus-0.02.tar.gz
    gitpan-cpan-author:       DAMI
    gitpan-cpan-maturity:     released

--- lib/Lingua/Thesaurus.pm
+++ lib/Lingua/Thesaurus.pm
@@ -5,7 +5,7 @@ use Module::Load ();
 use Carp;
 use namespace::clean -except => 'meta';
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 has 'storage'          => (is => 'ro', does => 'Lingua::Thesaurus::Storage',
                            handles => [qw/search_terms fetch_term
